a049281749a112957231e4292150a0f15225640b
sielbien7 <cnahueldasilva@gmail.com>
2026-07-01 15:57:51 -0300

    Setup inicial spring boot!

---
+++ src/test/java/tarea/climalert/ClimalertApplicationTests.java
@@ -0,0 +1,13 @@
+package tarea.climalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
